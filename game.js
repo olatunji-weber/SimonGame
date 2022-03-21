ef4fc740078b6d948c62a3cf9e91504fc04b67cc
Olatunji Olayinka <olatunji.weber@gmail.com>
2022-03-21 11:59:52 +0200

    Add files via upload

--- game.js
+++ game.js
@@ -1,18 +1,32 @@
-// $(document).click(function(){
-//   $("h1").text("I love programming so much.");
-//   $("h1").css("color","red");
-// });
-var buttonColours = ["red", "blue", "green", "yellow"];    //Crete an array of colours
-var gamePattern = [];                                      //Create an empty array
+//3. At the top of the game.js file, create a new array called buttonColours and set it to hold the sequence "red", "blue", "green", "yellow" .
+var buttonColours = ["red", "blue", "green", "yellow"];
 
-// Create a function to generate random numbers between 0 and 3
-function nextSequence(){
-  var randomNumber = Math.floor(Math.random() * 4);
-  return randomNumber;
+//5. At the top of the game.js file, create a new empty array called gamePattern.
+var gamePattern = [];
+var userClickedPattern = [];
+
+function userSequence(x){
+  // var userChosenColour = x;
+  userClickedPattern.push(x);
+  alert(userClickedPattern);
 }
 
-// Choose a random colour from the array of colours
-var randomChosenColour = buttonColours[nextSequence()];
+// var buttons = $(".btn"); //returns an array of the button objects
+$(".btn").click(function(){
+  userSequence( $(this).attr("id") );
+});
 
-// Create a pattern by pushing random colors to the array
-gamePattern.push(randomChosenColour);
+//1. Inside game.js create a new function called nextSequence()
+function nextSequence() {
+  //2. Inside the new function generate a new random number between 0 and 3, and store it in a variable called randomNumber
+  //4. Create a new variable called randomChosenColour and use the randomNumber from step 2 to select a random colour from the buttonColours array.
+  var randomChosenColour = buttonColours[Math.floor(Math.random() * 4)];
+  //6. Add the new randomChosenColour generated in step 4 to the end of the gamePattern.
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  audio.play();
+}
+
+nextSequence();
